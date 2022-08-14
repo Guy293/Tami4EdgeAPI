301f12b052908ce27a0f3a408bac6b2b05854464
guy shefer <g.shefer29@gmail.com>
2022-08-14 06:35:21 +0300

    Add README

---
+++ README.md
@@ -0,0 +1,1 @@
+# Tami 4 Edge / Edge+ API in Python
--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 import setuptools
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setuptools.setup(
     name="Tami4EdgeAPI",
     version="1.0.0",
     author="Guy Shefer",
     license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/Guy293/Tami4EdgeAPI",
     packages=setuptools.find_packages(),
     install_requires=["requests", "pypasser"],
