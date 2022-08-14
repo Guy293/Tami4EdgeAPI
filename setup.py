15581bd825e03cfd450b3343186f5ef4e6feee76
guy shefer <g.shefer29@gmail.com>
2022-08-14 06:20:29 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="Tami4EdgeAPI",
+    version="1.0.0",
+    author="Guy Shefer",
+    license="MIT",
+    url="https://github.com/Guy293/Tami4EdgeAPI",
+    packages=setuptools.find_packages(),
+    install_requires=["requests", "pypasser"],
+)
